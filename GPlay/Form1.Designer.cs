156ae7a8440f7f1d8bf1d4f2d4ee0dbd4f630b5a
PrankSinatra <7526868+vorlonempire@users.noreply.github.com>
2022-11-26 13:53:06 +0100

    Adding opening file button and Displaying name ina textbox

--- GPlay/Form1.Designer.cs
+++ GPlay/Form1.Designer.cs
@@ -28,20 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tB_currentTrack = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(389, 42);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tB_currentTrack
+            // 
+            this.tB_currentTrack.Location = new System.Drawing.Point(12, 44);
+            this.tB_currentTrack.Name = "tB_currentTrack";
+            this.tB_currentTrack.Size = new System.Drawing.Size(371, 20);
+            this.tB_currentTrack.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "GPlay";
+            this.Controls.Add(this.tB_currentTrack);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "Form1";
             this.Text = "GPlay";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tB_currentTrack;
     }
 }
 
--- GPlay/Form1.cs
+++ GPlay/Form1.cs
@@ -16,5 +16,35 @@ namespace GPlay
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openMusicFile = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Music Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = ".mp3",
+                Filter = "mp3 files (*.mp3)|*.mp3",
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+
+            };
+
+            if (openMusicFile.ShowDialog() == DialogResult.OK) 
+            { 
+
+                tB_currentTrack.Text= openMusicFile.FileName;   
+                //TODO : display name of the track only
+                //          now it displays whole path
+            }
+        }
     }
 }
