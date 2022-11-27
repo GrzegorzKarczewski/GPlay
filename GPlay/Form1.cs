71eff422cb9264b4e999673c424e3a41494e7160
PrankSinatra <7526868+vorlonempire@users.noreply.github.com>
2022-11-27 11:07:57 +0100

    if condition to prevent saving empty playlists

--- GPlay/Form1.cs
+++ GPlay/Form1.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Configuration;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 
@@ -193,13 +191,11 @@ namespace GPlay
             {
 
                 selectedFolder = playlistFolder.SelectedPath;
-                //foreach (var playlistItem in selectedFolder)
-                //{
                 string[] results = Directory.GetFileSystemEntries(selectedFolder, "*.mp3", SearchOption.AllDirectories); // TODO:: reads without keeping catalogue structure
-                         // .Select(file => Path.GetFullPath(file)).ToArray();
-                    playlistBox.Items.AddRange(results);
+                                                                                                                      // .Select(file => Path.GetFullPath(file)).ToArray();
+                playlistBox.Items.AddRange(results);
 
-               // }
+                
             }
 
         }
@@ -231,40 +227,30 @@ namespace GPlay
             saveFileDialog1.RestoreDirectory = true;
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-
-
-                //using (FileStream fs = File.Create(saveFileDialog1.FileName))
-                //{
-                //    foreach (var item in playlistBox.Items)
-                //    {
-                //        var length = item.ToString().Length;
-                //        Byte[] playlistContent = new UTF8Encoding(true).GetBytes(item.ToString());
-                //        fs.Write(playlistContent, 0, length);
-
-
-                //    }
-                //}
-
-                // another way
                 try
                 {
                     using (FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write))
                     {
                         StreamWriter write = new StreamWriter(fs);
-                        foreach (var item in playlistBox.Items)
+                        if (playlistBox.Items.Count != 0)
                         {
-                            write.BaseStream.Seek(0, SeekOrigin.End);
-                            write.WriteLine(item.ToString());
-                            // must flush to save entire list
-                            // without flush it won't save everything
-                            write.Flush();
+                            foreach (var item in playlistBox.Items)
+                            {
+                                write.BaseStream.Seek(0, SeekOrigin.End);
+                                write.WriteLine(item.ToString());
+                                // must flush to save entire list
+                                // without flush it won't save everything
+                                write.Flush();
+                            }
+                            fs.Close();
                         }
-                        fs.Close();
+                        MessageBox.Show("ERROR: Playlist Empty");
+
                     }
                 }
                 catch
                 {
-                    MessageBox.Show("ERROR Saving Playlist");
+                    MessageBox.Show("ERROR: Saving Playlist");
                 }
                 // Saving playlist name to configfile
                 try
@@ -290,7 +276,7 @@ namespace GPlay
         {
             if (playlistBox.Items.Count > 0)
             {
-                    playlistBox.Items.Clear();
+                playlistBox.Items.Clear();
             }
         }
     }
